d6d859282e13358fdc02d9ca9fe9beb7f4137ebd
javierclavijo <javierclavijog@gmail.com>
2021-01-25 19:29:43 +0100

    Add exercises 2.1-2.5

---
+++ part2/src/components/Course.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((p) => (
+        <Part part={p} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce(
+    (accumulator, part) => accumulator + part.exercises,
+    0
+  );
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+);
+
+export default Course;
